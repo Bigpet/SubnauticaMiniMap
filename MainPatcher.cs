cd01eae325ab90c0f036833d4f1f23e6cd1487e4
Peter Tissen <dravorek@gmail.com>
2018-11-02 21:43:18 +0100

    try instantiating on scene change

--- MainPatcher.cs
+++ MainPatcher.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using Harmony;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 namespace SubnauticaMiniMap
 {
@@ -11,10 +13,24 @@ namespace SubnauticaMiniMap
     {
         public static void Patch()
         {
-            var harmony = HarmonyInstance.Create("com.oldark.subnautica.acceleratedstart.mod");
+            var harmony = HarmonyInstance.Create("de.petertissen.subnautica.minimap.mod");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-            string[] lines = { "First line", "Second line", "Third line" };
-            System.IO.File.WriteAllLines(@"H:\SteamLibrary\steamapps\common\Subnautica\QMods\SubnauticaMap\Bars_dmp\WriteLines.txt", lines);
+            SceneManager.sceneLoaded += MainPatcher.OnSceneLoaded;
+            f = System.IO.File.Open(@"H:\SteamLibrary\steamapps\common\Subnautica\QMods\SubnauticaMap\Bars_dmp\modtest.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            sw = new StreamWriter(f);
+            sw.WriteLine("ModStart");
+            sw.Flush();
         }
+
+        private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "Main")
+            {
+                MiniMap.Load();
+            }
+        }
+
+        public static FileStream f;
+        public static StreamWriter sw;
     }
 }
---
+++ MiniMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SubnauticaMiniMap
+{
+    public class MiniMap : MonoBehaviour
+    {
+        public static void Load()
+        {
+            MainPatcher.sw.WriteLine("MiniMap Load");
+            MainPatcher.sw.Flush();
+            objs.Add(new GameObject("SubnauticaMiniMap.MiniMap").AddComponent<MiniMap>().gameObject);
+        }
+
+        private void Awake()
+        {
+            MainPatcher.sw.WriteLine("MiniMap Awake");
+            MainPatcher.sw.Flush();
+            MiniMap.Instance = this;
+        }
+
+        private Graphic g;
+
+        public static List<UnityEngine.Object> objs;
+
+        public static MiniMap Instance { get; private set; }
+    }
+}
