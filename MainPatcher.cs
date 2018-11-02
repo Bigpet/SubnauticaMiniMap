eccb8e4cb5dd4b2d6eeb08e7dce3d9a4fcca3c0f
Peter Tissen <dravorek@gmail.com>
2018-11-02 19:23:52 +0100

    Add hello world mod example

---
+++ MainPatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Harmony;
+
+namespace SubnauticaMiniMap
+{
+    class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.oldark.subnautica.acceleratedstart.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            string[] lines = { "First line", "Second line", "Third line" };
+            System.IO.File.WriteAllLines(@"H:\SteamLibrary\steamapps\common\Subnautica\QMods\SubnauticaMap\Bars_dmp\WriteLines.txt", lines);
+        }
+    }
+}
